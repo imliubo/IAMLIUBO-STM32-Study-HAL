f4c94e230c4f0aefd02b288152eade2e67a8cbf6
imliubo <imliubo@makingfun.xyz>
2018-09-20 16:23:19 +0800

    STM32F4xx_USART

---
+++ 4.STM32F407ZGT6_USART/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'usart' 
+ * Target:  'usart' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
